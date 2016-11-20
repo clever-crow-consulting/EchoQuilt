678220fe92325f49e0ad14b8196168edafe4dbf7
Dan Dye <root@clevercrowconsulting.com>
2016-11-20 16:22:56 -0500

    switch from Secrets() to env vars

--- send_sms.py
+++ send_sms.py
@@ -1,13 +1,14 @@
+import os
+
 # Download the twilio-python library from http://twilio.com/docs/libraries
 from twilio.rest import TwilioRestClient
 
-from secrets import Secrets
 
 # Find these values at https://twilio.com/user/account
-account_sid = Secrets().twilio_account_sid # "ACXXXXXXXXXXXXXXXXX"
-auth_token = Secrets().twilio_auth_token # "YYYYYYYYYYYYYYYYYY"
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")  # "ACXXXXXXXXXXXXXXXXX"
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")  # "YYYYYYYYYYYYYYYYYY"
 client = TwilioRestClient(account_sid, auth_token)
 
-message = client.messages.create(to=Secrets().dandye_google_voice,
-                                 from_=Secrets().twilio_phone_number,
+message = client.messages.create(to=os.environ.get("DANDYE_GOOGLE_VOICE"),
+                                 from_=os.environ.get("TWILIO_PHONE_NUMBER"),
                                  body="Treasure Tampa")
